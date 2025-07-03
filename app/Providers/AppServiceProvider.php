a01ed8e0e01361157dce35217e4ab5e4c8b48053
Alessandro Feitoza <alessandrofeitoza01@gmail.com>
2025-07-02 23:09:18 -0300

    Configure layers for system design

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,12 +4,15 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use App\Domain\Repository\ExampleRepository;
+use App\Domain\Repository\RepositoryInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
+        $this->app->bind(RepositoryInterface::class, ExampleRepository::class);
     }
 
     public function boot(): void
