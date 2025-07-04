971772de67ecf50b7de157903dbec15fce63f49d
Alessandro Feitoza <alessandrofeitoza01@gmail.com>
2025-07-04 00:07:47 -0300

    Add and configure swagger api docs (#16)

---
+++ public/docs/assets/js/swagger-initializer.js
@@ -0,0 +1,26 @@
+
+
+const mountSwagger = new Promise((resolve, reject) => {
+    resolve(SwaggerUIBundle({
+        url: './openapi.yaml',
+        dom_id: '#swagger-ui',
+        deepLinking: true,
+        presets: [
+            SwaggerUIBundle.presets.apis,
+            SwaggerUIStandalonePreset
+        ],
+        plugins: [
+            SwaggerUIBundle.plugins.DownloadUrl,
+        ],
+        layout: "StandaloneLayout",
+    }));
+})
+
+window.onload = function () {
+    document.querySelector('.topbar').style.display = 'none';
+
+    mountSwagger
+        .then((res) => {
+            window.ui = res;
+        });
+};
